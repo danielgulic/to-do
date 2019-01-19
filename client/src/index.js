c111b0fbfc8c59b937779ae41aa5d43e85a6b785
Daniel Gulic <danielgulic@gmail.com>
2019-01-19 13:01:37 +1100

    snackbar init

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SnackbarProvider } from 'notistack';
 import './index.css';
 import App from './App';
 import { API_BASE } from './config';
 
 console.log('Using API base ' + API_BASE);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SnackbarProvider maxSnack={3}><App /></SnackbarProvider>, document.getElementById('root'));
